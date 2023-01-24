f5eff8b4c1041f651da5716ccd2c02463d1c3325
Rakesh <mandalrakesh628@gmail.com>
2023-01-24 20:57:44 +0530

    improved some functionalities

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-let n = Math.round(100 * Math.random());
-// alert(n);
+function calculate() {
+    let loveScore = Math.round(100 * Math.random()) + 1;
+    let scoreText = "You may find your partner this month.";
+    if (loveScore < 35) {
+        scoreText = "Hey buddy, you're gonna stay Single again."
+    } else if (loveScore >= 35 && loveScore < 75) {
+        scoreText = "Don't worry, you may find your partner this time."
+    } else {
+        scoreText = "Congrats! there's a very high probability you'll find your partner this time."
+    }
+
+    // expression = document.getElementById("expression").value
+    // console.log(expression)
+    // if (expression) {
+    //     try {
+    //         answer = eval(expression)
+    //         document.getElementById("expression").value = answer
+    //         addlog(expression + " = " + answer)
+    //     } catch(e) {
+    //         console.log("Bad Expression")
+    //         addlog("Expression '" + expression + "' is a Bad Expression. Please Fix it.")
+    //     }
+    // }
+}
